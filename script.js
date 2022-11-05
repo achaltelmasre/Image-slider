176af13290095bf530944ccbba11cac03d1732e8
achaltelmasre <achaltelmasre@gmail.com>
2022-11-05 12:55:48 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let flag = 2;
+
+function controller(x){
+       flag = flag + x;
+       slideshow(flag);
+
+}
+
+slideshow(flag);
+
+function slideshow(num){
+    let slides = document.getElementsByClassName('slide');
+
+    if(num == slides.length){
+        flag = 0;
+        num = 0;
+
+    }
+
+    for(let y of slides){
+        y.style.display = "none"
+    }
+
+    slides[num].style.display = "block";
+
+}
